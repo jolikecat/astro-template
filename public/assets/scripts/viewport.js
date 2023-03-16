6d86313623712ac0a9525e69e335fbc6719cf49c
jolikecat <jkawakubo@contenna.jp>
2023-03-16 18:50:52 +0900

    共通のCSS,JS,astroコンポーネントを作成

---
+++ public/assets/scripts/viewport.js
@@ -0,0 +1,13 @@
+const minWidth = 375;
+const el = document.querySelector('meta[name="viewport"]');
+const updateContent = () => {
+	if (window.screen.width < minWidth) {
+		el.setAttribute('content', `width=${minWidth}`);
+	} else {
+		el.setAttribute('content', 'width=device-width');
+	}
+};
+
+updateContent();
+
+window.addEventListener('orientationchange', updateContent);
