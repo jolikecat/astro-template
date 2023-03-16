100e357f2ce516068db8348ac28fc096f99d9d8d
jolikecat <jkawakubo@contenna.jp>
2023-03-16 18:51:12 +0900

    ビルド後のディレクトリ設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,33 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com/",
+    site: 'https://example.com/',
+    build: {
+        format: 'file',
+        assets: 'assets/scripts'
+    },
+    vite: {
+        build: {
+            assetsInlineLimit: 0,
+            rollupOptions: {
+            output: {
+                entryFileNames: `assets/scripts/[name].js`,
+                // chunkFileNames: `assets/scripts/[name].js`,
+                assetFileNames: assetInfo => {
+                let extType = path.extname(assetInfo.name);
+                console.log(assetInfo);
+                if (/css/.test(extType)) {
+                    extType = 'styles';
+                }
+                if (/jpg|jpeg|png|webp|svg|gif|avif/.test(extType)) {
+                    extType = 'images';
+                }
+                return `assets/${extType}/[name][extname]`;
+                }
+            }
+            }
+        }
+    },
 });
