8146f6de363e5368e4f095fb4e8c68ad4d2dd805
jolikecat <jkawakubo@contenna.jp>
2023-04-14 11:33:41 +0900

    astro-compressを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,49 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com/',
     build: {
-        format: 'file',
-        assets: 'assets/scripts'
+        format: 'file'
     },
     vite: {
         build: {
             assetsInlineLimit: 0,
             rollupOptions: {
-            output: {
-                entryFileNames: `assets/scripts/[name].js`,
-                // chunkFileNames: `assets/scripts/[name].js`,
-                assetFileNames: assetInfo => {
-                let extType = path.extname(assetInfo.name);
-                console.log(assetInfo);
-                if (/css/.test(extType)) {
-                    extType = 'styles';
+                output: {
+                    entryFileNames: `assets/scripts/[name].js`,
+                    assetFileNames: assetInfo => {
+                        let extType = path.extname(assetInfo.name);
+
+                        if (/css/.test(extType)) {
+                            extType = 'styles';
+                        }
+                        if (/jpg|jpeg|png|webp|svg|gif|avif/.test(extType)) {
+                            extType = 'images';
+                        }
+                        return `assets/${extType}/[name][extname]`;
+                    }
                 }
-                if (/jpg|jpeg|png|webp|svg|gif|avif/.test(extType)) {
-                    extType = 'images';
-                }
-                return `assets/${extType}/[name][extname]`;
-                }
-            }
             }
         }
     },
+    integrations: [
+        compress({
+            css: false,
+            html: {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: false,
+                removeComments: false,
+                removeEmptyAttributes: false,
+                removeRedundantAttributes: false,
+                sortAttributes: false,
+                sortClassName: false,
+            },
+            img: false,
+            js: false,
+            svg: false,
+        })
+    ]
 });
