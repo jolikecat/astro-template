d6978e39d75715578104796ada308f905ee640c2
jolikecat <jkawakubo@contenna.jp>
2023-04-17 17:35:37 +0900

    :adhesive_bandage: astroの設定ファイルを変更、previewに--hostを追加、viewport.jsの内容を移動

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,26 @@ import HtmlCssFormatter from './integrations/html-css-formatter'
 export default defineConfig({
     site: 'https://example.com/',
     build: {
-        format: 'file'
+        format: 'file',
+        assets: 'assets/scripts',
     },
     vite: {
         build: {
             assetsInlineLimit: 0,
             rollupOptions: {
                 output: {
-                    entryFileNames: `assets/scripts/[name].js`,
                     assetFileNames: assetInfo => {
                         let extType = path.extname(assetInfo.name);
 
                         if (/css/.test(extType)) {
                             extType = 'styles';
                         }
+
                         if (/jpg|jpeg|png|webp|svg|gif|avif/.test(extType)) {
                             extType = 'images';
                         }
-                        return `assets/${extType}/[name][extname]`;
+                        
+                        return `assets/${extType}/[name].[hash][extname]`;
                     }
                 }
             }
