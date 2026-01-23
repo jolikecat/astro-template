c52689937b5ee889f20cb97ff02841f2b9973953
jolikecat <jkawakubo@contenna.jp>
2026-01-23 14:01:43 +0900

    chore: huskyとlint-stagedを導入

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+export default {
+	'*.{js,ts,astro,vue}': 'prettier --write',
+	'*.{css,scss}': 'prettier --write',
+	'*.{json,md,yml,yaml}': 'prettier --write',
+};
--- prettier.config.js
+++ prettier.config.js
@@ -8,6 +8,7 @@ const config = {
 	singleQuote: true,
 	trailingComma: 'all',
 	useTabs: true,
+	htmlWhitespaceSensitivity: 'ignore',
 	plugins: ['prettier-plugin-astro'],
 	overrides: [
 		{
